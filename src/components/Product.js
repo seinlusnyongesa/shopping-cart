bba3344b4538466768af0b70cc20d679a970d742
seinlusnyongesa <seinlus044@gmail.com>
2022-11-04 04:38:15 -0700

    Add fetch data functionality

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,15 +1,16 @@
 import React from "react";
-import TestImg from "../assets/images/swag.jpg";
 
-const Product = () => {
+const Product = ({ product }) => {
+  const { title, price, description, image } = product;
+
   return (
-    <div className="border border-slate-300 mx-5 rounded-t-md">
-      <div className="mb-4">
-        <img src={TestImg} alt="test" className="rounded-t-md" />
+    <div className="border border-slate-300 m-5 rounded-t-md">
+      <div className="mb-4 h-32 mt-4 flex justify-center items-center">
+        <img src={image} alt={description} className="rounded-t-md h-full" />
       </div>
       <div className="flex flex-col mx-4 border-t border-slate-500">
-        <h1 className="font-bold capitalize my-2">name of clothing</h1>
-        <h2 className="font-semibold">price</h2>
+        <h1 className="font-bold capitalize my-2">{title}</h1>
+        <h2 className="font-semibold">${price}</h2>
         <button className="bg-slate-800 hover:bg-slate-500 text-slate-50 font-bold p-2 my-4">
           Add to cart
         </button>
--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,13 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Product from "../components/Product";
 
 const Products = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchDAta = async () => {
+      let res = await fetch("https://fakestoreapi.com/products");
+      const data = await res.json();
+      setProducts(data);
+    };
+
+    fetchDAta();
+  }, []);
+
   return (
-    <div className="grid gap-4 mt-6 max-w-5xl mx-auto grid-cols-fluid">
-      <Product />
-      <Product />
-      <Product />
-    </div>
+    <>
+      {products.length > 0 ? (
+        <div className="grid gap-4 mt-6 max-w-5xl mx-auto grid-cols-fluid">
+          {products.map((product) => (
+            <Product key={product.id} product={product} />
+          ))}
+        </div>
+      ) : (
+        <div className="h-[calc(100vh-4.5rem)] w-screen flex justify-center items-center font-bold text-2xl">
+          loading...
+        </div>
+      )}
+    </>
   );
 };
 
